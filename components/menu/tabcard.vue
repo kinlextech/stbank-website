37d08e7c8a7a68a75aee7a9e679f0a35541cbc04
kinlextech <laithong@STB.LA>
2021-04-22 17:51:51 +0700

    change

---
+++ components/menu/tabcard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <button class="change__style" @click="changeStyle()">Change Style</button>
+    <st-tabs :mode="mode">
+      <st-tab title="Tab 1">Hello From Tab 1</st-tab>
+      <st-tab title="Tab 2">Hello From Tab 2</st-tab>
+      <st-tab title="Tab 3">Hello From Tab 3</st-tab>
+      <st-tab title="Tab 4">Hello From Tab 4</st-tab>
+    </st-tabs>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- pages/services/fundtransfer/index.vue
+++ pages/services/fundtransfer/index.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
-    <carousel-client />
-    <div class="container">
+    <div class="bg-primary">
+      <div class="container">
+        <vs-row>
+          <vs-col w="6">
+            <h2 class="text-center">ລະບົບບໍລິການໂອນເງິນ ຂອງ ທອທ</h2>
+          </vs-col>
+          <vs-col w="6">
+            <img
+              src="https://www.federalbank.co.in/image/journal/article?img_id=29432160&t=1607455453426&fileName=18237092"
+            />
+          </vs-col>
+        </vs-row>
+      </div>
+    </div>
+    <div class="container pt-5 pb-5">
       <vs-row aligin="center" justify="center">
-        <vs-col w="4" v-for="(itr,index) in page" :key="index">
+        <vs-col w="4" v-for="(itr, index) in page" :key="index">
           <vs-card>
             <template #title>
-              <h3>{{itr.title}}</h3>
+              <h3>{{ itr.title }}</h3>
             </template>
             <template #img>
               <img src="/foto5.png" alt="" />
             </template>
             <template #text>
-              <p>{{itr.description}}</p>
+              <p>{{ itr.description }}</p>
               <a href="#">View More</a>
             </template>
           </vs-card>
